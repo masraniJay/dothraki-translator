7f726651c89fb061e776b2fb9ca44146c9351782
masraniJay <76557175+masraniJay@users.noreply.github.com>
2021-09-24 16:01:20 +0530

    done

--- app.js
+++ app.js
@@ -7,4 +7,22 @@ function constructUrl(input){
     serverUrl = serverUrl + "?" + input;
 }
 
-function
+function getTranslationURL(input){
+    return serverUrl + "?" + "text" + input
+}
+
+function errorHandler(error){
+    console.log("error occured", error)
+    alert("Something wrong with the server, try again in some time!")
+}
+
+function clickHandler(){
+    inputTxt = txtInput.value
+    fetch(constructUrl(inputTxt))
+    .then(response => (response.json))
+    .then(outputDiv.innertext = json.contents.translated)
+
+    .catch(errorHandler);   
+}
+
+btnTranslate.addEventListener("click", clickHandler())
